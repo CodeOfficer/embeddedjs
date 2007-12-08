f908dff15a8bf1521e47bb963db3eac3eb277975
justinbmeyer <justinbmeyer@efe5f0e3-6940-0410-82d6-07539bb1838e>
2007-12-08 02:12:43 +0000

    cross browser, prototype-less Ajax templates added

--- src/templates.js
+++ src/templates.js
@@ -23,14 +23,13 @@ Templates.prototype = {
 		if (template) return template;
 	    if (template == this.INVALID_PATH) return null;
 		
-		var response = new Ajax.Request(path, {asynchronous: false, method: "get"});
-
-		if ( response.transport.status == 404 || 
-			(response.transport.status == 0 && response.transport.responseText == '') ) { // this means the template was not found
-	        this.update(path, this.INVALID_PATH);
+		var text = Templates.request(path)
+		if(text == null){
+			this.update(path, this.INVALID_PATH);
 	        return null;
-	    }
-	    var template = new EjsCompiler(response.transport.responseText, this.type);
+		}
+
+	    var template = new EjsCompiler(text, this.type);
 		template.path = path;
 	    try {
 			template.compile();
@@ -43,7 +42,34 @@ Templates.prototype = {
 		return template;
 	}
 }
-
-
+if(typeof Prototype != 'undefined') {
+	Templates.request = function(path){
+		var response = new Ajax.Request(path, {asynchronous: false, method: "get"});
+		if ( response.transport.status == 404 || response.transport.status == 2 ||
+			(response.transport.status == 0 && response.transport.responseText == '') ) return null;
+	    return response.transport.responseText
+	}
+}else
+{
+	Templates.request = function(path){
+	   var factories = [function() { return new XMLHttpRequest(); },function() { return new ActiveXObject("Msxml2.XMLHTTP"); },function() { return new ActiveXObject("Microsoft.XMLHTTP"); }];
+	   var request
+	   for(var i = 0; i < factories.length; i++) {
+	        try {
+	            request = factories[i]();
+	            if (request != null)  break;
+	        }
+	        catch(e) { continue;}
+	   }
+	   request.open("GET", path, false);
+	   
+	   try{request.send(null);}
+	   catch(e){return null;}
+	   
+	   if ( request.status == 404 || request.status == 2 ||(request.status == 0 && request.responseText == '') ) return null;
+	   
+	   return request.responseText
+	}
+}
 
 
